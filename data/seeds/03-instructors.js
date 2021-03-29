f4b3023e36c6725c9ceebf4c23b9436dffc5ee3b
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-03-29 13:57:28 -0500

    heroku push

--- data/seeds/03-instructors.js
+++ data/seeds/03-instructors.js
@@ -1,19 +1,19 @@
 exports.seed = function(knex) {
   return knex("instructors").insert([
     {
-      id:1,
+      instructor_id:1,
       username:"DavidGoggins",
       password:"chicken1",
       role:1
     },
     {
-      id:2,
+      instructor_id:2,
       username:"Instructor2",
       password:"NoPain123",
       role:1
     },
     {
-      id:3,
+      instructor_id:3,
       username:"kdolic123",
       password:"lifting123",
       role:1
--- data/seeds/04-clients.js
+++ data/seeds/04-clients.js
@@ -1,19 +1,19 @@
 exports.seed = function(knex) {
   return knex("clients").insert([
     {
-      id:1,
+      client_id:1,
       username:"KenanDolic1",
       password:"newGains",
       role:2
     },
     {
-      id:2,
+      client_id:2,
       username:"Lifter2",
       password:"password123",
       role:2
     },
     {
-      id:3,
+      client_id:3,
       username:"protein4days",
       password:"nocarbs",
       role:2
